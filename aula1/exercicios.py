3e5cfe3eef0b1e57c0a616ed2bb673e6dea282fe
vitor-kb <vitorbassani1@gmail.com>
2023-10-21 10:36:24 -0300

    adding exercicio 2

--- aula1/exercicios.py
+++ aula1/exercicios.py
@@ -1,14 +1,27 @@
-#1) Fazer um programa para ler o nome do aluno, ler 4 notas e calcular a média e imprimir
+#1) Fazer um programa para ler o nome do aluno, ler 4 notas e calcular a média e imprimir.
 
-nome = input("Digite o nome do aluno: ");
+"""nome = input("Digite o nome do aluno: ");
 
 nota1 = float(input("Insira a 1ª nota: "));
 nota2 = float(input("Insira a 2ª nota: "));
 nota3 = float(input("Insira a 3ª nota: "));
 nota4 = float(input("Insira a 4ª nota: "));
 
-média = (nota1 + nota2 + nota3 + nota4)/4;
+media = (nota1 + nota2 + nota3 + nota4)/4;
 
-print(f"A média deste aluno é {média}");
+print('Nome do Aluno: ', nome);
+print('Media do Aluno: ', media);"""
+
+#2) Fazer um programa para ler a distancia, valor do litro da gasolina, consumo do carro.
+
+distancia = float(input('Digite a distancia percorrida do veiculo: '));
+
+gasosa = float(input('Digite o valor do litro da gasolina: '));
+
+consumo = float(input('Digite o valor de consumo do veiculo: '));
+
+gasto_total = gasosa * (consumo * distancia);
+
+print(f'Seu gasto será o equivalente a R$ {gasto_total}');
 
 
