9018e51d042c23d7773403b72486f4ce8d5acb98
vitor-kb <vitorbassani1@gmail.com>
2023-10-28 08:33:21 -0300

    atualizando código da aula2

---
+++ aula2/aula2.py
@@ -0,0 +1,27 @@
+#estruturas condicionais
+a=10;
+b=10;
+c=11;
+if a>b:
+    print('b é maior que a');
+elif b == a:
+    print('a e b são iguais');
+else:
+    print('b é menor que a');
+
+#estruturas condicionais²
+var1 = 18;
+var2 = 2;
+var3 = "Maria";
+var4 = 4;
+
+if var2 > var1:
+    print("var1 é maior que var 2");
+elif var2 == 500:
+    print("var2 vale 500");
+elif var3 == var2:
+    print("var3 e var2 são iguais");
+elif var4 is str("4"):
+    print("var4 não é do tipo string");
+else:
+    print("Nenhuma condição é verdadeira!");
